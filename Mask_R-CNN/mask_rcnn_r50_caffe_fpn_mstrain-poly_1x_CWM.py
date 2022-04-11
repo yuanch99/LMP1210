58bec78b6f076bc2e0b42a291758383b4571d7c6
PeterHHer <70668756+PeterHHer@users.noreply.github.com>
2022-04-10 23:32:09 -0400

    Add files via upload

---
+++ Mask_R-CNN/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_CWM.py
@@ -0,0 +1,29 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '/content/mmdetection/configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=1),
+        mask_head=dict(num_classes=1)))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ('GCI',)
+data = dict(
+    train=dict(
+        img_prefix='/content/drive/MyDrive/Colab Notebooks/CWM/CWM Train IMG',
+        classes=classes,
+        ann_file='/content/drive/MyDrive/Colab Notebooks/CWM/CWM_Train_ann.json'),
+    val=dict(
+        img_prefix='/content/drive/MyDrive/Colab Notebooks/CWM/CWM Val IMG',
+        classes=classes,
+        ann_file='/content/drive/MyDrive/Colab Notebooks/CWM/CWM_Val_ann.json'),
+    test=dict(
+        img_prefix='/content/drive/MyDrive/Colab Notebooks/CWM/CWM Test IMG/',
+        classes=classes,
+        ann_file='/content/drive/MyDrive/Colab Notebooks/CWM/CWM_Test_ann.json'))
+
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = "/content/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth"
